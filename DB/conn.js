5ed4ebbe571e4a993d3d62cae06ffdae57422227
akash_ghadge <asghadge6@gmail.com>
2022-12-12 16:30:30 +0530

    URI added

--- DB/conn.js
+++ DB/conn.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI;
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/yoga-class", {
+mongoose.connect(uri || "mongodb://127.0.0.1:27017/yoga-class", {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then((data) => {
     console.log("DB is connected..");
